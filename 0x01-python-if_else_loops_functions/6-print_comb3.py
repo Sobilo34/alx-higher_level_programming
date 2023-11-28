cfb3e5aff0b88952014bc904b8eefa09a76c7e96
Sobilo34 <bilalsolih60@gmail.com>
2023-11-28 06:01:13 -0800

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for count in range(0, 10):
-    for iterate in range(0, 10):
-        if count != iterate and iterate > count and count < 8:
+    for iterate in range(count + 1, 10):
+        if count < 8:
             print("{}{}, ".format(count, iterate), end='')
-        elif count != iterate and iterate > count and count > 7:
+        elif count > 7:
             print("{}{}".format(count, iterate))
