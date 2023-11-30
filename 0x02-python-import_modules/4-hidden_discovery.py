97544422e8186961a0446bdbf2e2b5aa80204dec
Sobilo34 <bilalsolih60@gmail.com>
2023-11-30 05:45:37 -0800

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 
+import importlib
+
 if __name__ == "__main__":
-    import importlib
-    
+    _import = importlib.import_module("hidden_4")
+
+    byte = dir(_import)
+
+    for index, the_name in enumerate(byte):
+        if the_name[0] == '_':
+            continue
+
+        print("{}".format(the_name))
