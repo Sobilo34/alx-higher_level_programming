7fe45c472b6a8094b8d944f26fbfbcd78bf2c326
Sobilo34 <bilalsolih60@gmail.com>
2023-11-30 09:12:56 -0800

    102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,6 +2,7 @@
 
 from magic_calculation_102 import add, sub
 
+
 def magic_calculation(a, b):
     if a < b:
         c = add(a, b)
@@ -10,4 +11,3 @@ def magic_calculation(a, b):
         return c
     else:
         return sub(a, b)
-
