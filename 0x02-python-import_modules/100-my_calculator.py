2a52e1e30c5afba6689a946c1b21e6202200b311
Sobilo34 <bilalsolih60@gmail.com>
2023-11-30 08:07:09 -0800

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,21 +5,22 @@ if __name__ == "__main__":
     from sys import argv, exit
 
     count = len(argv)
-    if count < 4 or > 4:
-            print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-            exit(1)
+    if count < 4 or count > 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
     else:
         nul = [None] * 4
         for index in range(1, count):
             nul[index] = argv[index]
 
-         a = "{} + {} = {}".format(nul[1], nul[3], add(int(nul[1]), int(nul[3])))
-         b = "{} + {} = {}".format(nul[1], nul[3], sub(int(nul[1]), int(nul[3])))
-         c = "{} + {} = {}".format(nul[1], nul[3], mul(int(nul[1]), int(nul[3])))
-         d = "{} + {} = {}".format(nul[1], nul[3], div(int(nul[1]), int(nul[3])))
+        a = "{} + {} = {}".format(nul[1], nul[3], add(int(nul[1]), int(nul[3])))
+        b = "{} + {} = {}".format(nul[1], nul[3], sub(int(nul[1]), int(nul[3])))
+        c = "{} + {} = {}".format(nul[1], nul[3], mul(int(nul[1]), int(nul[3])))
+        d = "{} + {} = {}".format(nul[1], nul[3], div(int(nul[1]), int(nul[3])))
+        
         if nul[2] == '+':
             print(a)
-        elif nul[2] == '=':
+        elif nul[2] == '-':
             print(b)
         elif nul[2] == '*':
             print(c)
@@ -28,3 +29,4 @@ if __name__ == "__main__":
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
+
