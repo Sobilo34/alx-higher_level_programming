27f869ec984da6a970ec4213f4c045c01b598d1f
Sobilo34 <bilalsolih60@gmail.com>
2023-11-30 05:16:48 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-   import sys 
+    from sys import argv
+
+    count = len(argv) - 1
+    if count == 1:
+        string = "argument"
+    else:
+        string = "arguments"
+
+    if count == 0:
+        split_operator = '.'
+    else:
+        split_operator = ':'
+
+    print("{} {}{}".format(count, string, split_operator))
+    for index in range(1, count + 1):
+        print("{}: {}".format(index, argv[index]))
