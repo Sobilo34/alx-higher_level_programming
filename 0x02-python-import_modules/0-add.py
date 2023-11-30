8937cceecfc71afdf66a79e9b25145bbbe669419
Sobilo34 <bilalsolih60@gmail.com>
2023-11-30 03:56:20 -0800

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,5 +5,6 @@ if __name__ == "__main__":
     a = 1;
     b = 2;
 
-    print(f"{a} + {b} = {add_0.add(a, b)}\n")
+    print("{} + {} = {}".format(a, b, add(a, b)))
+#print(f"{a} + {b} = {add_0.add(a, b)}\n")
 
