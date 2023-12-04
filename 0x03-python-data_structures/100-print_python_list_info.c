3a7983c0349d907fef99b7cb4262f44e79646829
Sobilo34 <bilalsolih60@gmail.com>
2023-12-04 11:53:53 -0800

    12-switch.py

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,36 @@
+#include <Python.h>
+
+
+/**
+ * print_python_list_info - This is a function that prints
+ * information about a Python list.
+ * @p: A pointer to a PyObject that represents a Python list
+ * Return: Void
+ */
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t size, alloc, a;
+
+	if (!PyList_Check(p))
+	{
+		fprintf(stderr, "Invalid Python list\n");
+		return;
+	}
+
+	size = PyList_Size(p);
+	alloc = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", alloc);
+
+	for (a = 0; a < size; a++)
+	{
+		PyObject *element = PyList_GetItem(p, a);
+
+		if (element != NULL)
+			printf("Element %ld: %s\n", a, Py_TYPE(element)->tp_name);
+		else
+			fprintf(stderr, "Failed to retrieve element %ld\n", a);
+	}
+}
+
