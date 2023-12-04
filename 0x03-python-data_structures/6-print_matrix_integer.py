ba267c5d3da105ca7ea9ba64fdda5b1d4543260e
Sobilo34 <bilalsolih60@gmail.com>
2023-12-04 08:52:05 -0800

    0x03 python data structure

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,10 @@
+#!/usr/bin/python3
 
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for a, value in enumerate(row):
+            if a == len(row) - 1:
+                print("{:d}".format(value), end="")
+            else:
+                print("{:d} ".format(value), end="")
+        print()
