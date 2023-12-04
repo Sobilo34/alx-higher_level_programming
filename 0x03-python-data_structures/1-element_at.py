781117b43db37a930b87f1a43b04fdb8fd0964ed
Sobilo34 <bilalsolih60@gmail.com>
2023-12-04 08:01:36 -0800

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,4 +4,5 @@ def element_at(my_list, idx):
     length = len(my_list)
     if 0 <= idx < length:
         return my_list[idx]
-    return None
+    else:
+        return None
---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    length = len(my_list)
+    if 0 <= idx < length:
+        my_list[idx] = element
+    else:
+        return my_list
