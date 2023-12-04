a0ca2bc6e24858a0510636a8395e2173bb17bc2e
Sobilo34 <bilalsolih60@gmail.com>
2023-12-04 08:09:22 -0800

    0x03 python daa structure

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,5 +4,4 @@ def replace_in_list(my_list, idx, element):
     length = len(my_list)
     if 0 <= idx < length:
         my_list[idx] = element
-    else:
-        return my_list
+    return my_list
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,5 +4,4 @@ def print_reversed_list_integer(my_list=[]):
     length = len(my_list)
     if 0 < idx < length:
         my_list[idx] = element
-    else:
-        return my_list
+    return my_list
