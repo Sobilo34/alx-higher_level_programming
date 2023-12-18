952d1136c1782e8103e76e3a710ef92364b401d8
Sobilo34 <bilalsolih60@gmail.com>
2023-12-18 05:32:40 -0800

    divides element by element 2 lists

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    output = None
+
+    try:
+        output = p / q
+    except ZeroDivisionError:
+        output = None
+    finally:
+        print("Inside result: {:}" .format(output))
+    return (output)
