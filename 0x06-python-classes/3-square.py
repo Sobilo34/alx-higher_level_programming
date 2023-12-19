87aa31ec5f55b9b071c501165e9f320d391cb9dc
Sobilo34 <bilalsolih60@gmail.com>
2023-12-19 06:35:14 -0800

    a class Square that defines a square by: (based on 2-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,14 @@
+class Square:
+    def __init__(self, size=0):
+        self.size = size
+
+    def area(self):
+        return self.size ** 2
+
+if __name__ == "__main__":
+    square1 = Square(5)
+    print("Area of square1:", square1.area())
+
+    square2 = Square()
+    print("Area of square2:", square2.area())
+
