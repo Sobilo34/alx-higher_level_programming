d821395c21b47bfbc64a37014800cd267bad513a
Sobilo34 <bilalsolih60@gmail.com>
2023-12-19 08:23:38 -0800

    a class Node that defines a node of a singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,52 +1,85 @@
 #!/usr/bin/python3
-"""This is to define a class node for singly linked list"""
+"""Define a class Node for singly linked list."""
 
 
 class Node:
-    """This is to represent node"""
+    """Represent a node in a singly linked list."""
 
     def __init__(self, data, next_node=None):
+        """Initialize a new Node.
+
+        Args:
+            data (int): The data of the node.
+            next_node (Node, optional): Next node in list. Defaults to None.
+        """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
+        """Get or set the data of the node."""
         return self.__data
 
     @data.setter
     def data(self, value):
+        """Set the data of the node.
+
+        Args:
+            value (int): The data of the node.
+
+        Raises:
+            TypeError: If value is not an integer.
+        """
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
+        """Get or set the next node."""
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
+        """Set the next node.
+
+        Args:
+            value (Node or None): The next node in the list.
+
+        Raises:
+            TypeError: If value is not a Node object or None.
+        """
         if value is not None and not isinstance(value, Node):
-            raise TypeError("next_node must be a Node object or None")
+            raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
 class SinglyLinkedList:
+    """Represent a singly linked list."""
+
     def __init__(self):
+        """Initialize an empty singly linked list."""
         self.head = None
 
     def sorted_insert(self, value):
+        """Insert a new Node into correct sorted position in list (asc order).
+
+        Args:
+            value (int): The data of the new node.
+        """
         new_node = Node(value)
         if self.head is None or value < self.head.data:
             new_node.next_node = self.head
             self.head = new_node
         else:
-            curr = self.head
+            curr = self.head  # curr -> current
             while curr.next_node is not None and curr.next_node.data < value:
                 curr = curr.next_node
             new_node.next_node = curr.next_node
             curr.next_node = new_node
 
     def __str__(self):
+        """Print the entire list."""
         result = []
         curr = self.head
         while curr is not None:
