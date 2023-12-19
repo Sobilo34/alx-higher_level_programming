7b8401420c9e057d85f9e21c0ff4e15cf6ea30c5
Sobilo34 <bilalsolih60@gmail.com>
2023-12-19 09:14:36 -0800

    the Python class MagicClass that imitate another python bytecode

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
 
 """This is to define MagicClass matching bytecode"""
-
-
 import math
 
+
 class MagicClass:
+    """
+    Initialize and define methods area and circumference
+    to represent a circle.
+    """
     def __init__(self, radius=0):
+        self.__radius = 0
+
+        #  verify that radius is a number
         if not isinstance(radius, (int, float)):
-            raise TypeError('radius must be a number')
-        self.radius = radius
+            raise TypeError("radius must be a number")
+        #  update attribute
+        self.__radius = radius
 
     def area(self):
-        return math.pi * self.radius ** 2
+        return self.__radius ** 2 * math.pi
 
     def circumference(self):
-        return 2 * math.pi * self.radius
+        return 2 * math.pi * self.__radius
