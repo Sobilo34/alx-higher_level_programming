e12ed550738b3ea65399d79b5935604034c37652
Sobilo34 <bilalsolih60@gmail.com>
2024-01-09 10:57:33 -0800

    9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,17 +1,18 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 
 """
-THis is a function that Write a class Student that defines a student
+This is a function that Write a class Student that defines a student
 """
 
 class Student:
     """
     class that defines a Student
     """
-def __init__(self, first_name, last_name, age):
-    """
-    Initilize the class
-    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initilize the class
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
