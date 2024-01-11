78210ea6fd9da4cedde28c41fc2194cf498d55a2
Sobilo34 <bilalsolih60@gmail.com>
2024-01-11 01:36:47 +0100

    1-my_list.t.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,23 +1,23 @@
-#!/usr/bin/python3 
- """ 
- Module: 1-my_list 
- Contains the definition of the MyList class. 
- """ 
-  
-  
- class MyList(list): 
-     """ 
-     A class that inherits from the list class with additional functionality. 
-     """ 
-  
-     def __init__(self): 
-         """ 
-         To initialize a new instance of MyList.
-         """ 
-         super().__init__() 
-  
-     def print_sorted(self): 
-         """ 
-         The functuon print the list, but sorted in ascending order. 
-         """ 
-         print(sorted(self))
+#!/usr/bin/python3
+"""
+Module: 1-my_list
+Contains the definition of the MyList class.
+"""
+
+
+class MyList(list):
+    """
+    A class that inherits from the list class with additional functionality.
+    """
+
+    def __init__(self):
+        """
+        To initialize a new instance of MyList.
+        """
+        super().__init__()
+
+    def print_sorted(self):
+        """
+        The function prints the list, but sorted in ascending order.
+        """
+        print(sorted(self))
