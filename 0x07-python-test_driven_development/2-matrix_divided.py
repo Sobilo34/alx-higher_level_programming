978ab2928df216d33ec630de60849d6724f944ee
Sobilo34 <bilalsolih60@gmail.com>
2024-01-11 01:20:03 +0100

    2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 
+"""
+This function divides all the elements of a matrix.
+"""
+
+
 def matrix_divided(matrix, div):
     """
-    Function that divides all elements of a matrix.
+    The function that divides all elements of a matrix.
 
     Args:
         matrix (list of lists): The matrix to be divided.
@@ -15,28 +20,39 @@ def matrix_divided(matrix, div):
         ZeroDivisionError: If div is equal to 0.
 
     Returns:
-        list of lists: A new matrix with elements divided by div, rounded to 2 decimal places.
+        list of lists: A new matrix with elements
+        divided by div, rounded to 2 decimal places.
     """
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    err_message = "The matrix must be a matrix
+    (i.e list of lists) of integers/floats"
 
-    if not all(len(row) == len(matrix[0]) for row in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
+    if matrix is None or type(matrix) is not list:
+        raise TypeError(err_message)
 
-    if not isinstance(div, (int, float)):
+    if type(matrix[0]) is not list:
+        raise TypeError(err_message)
+
+    matrix_len = len(matrix[0])
+
+    for row in matrix:
+        if len(row) != matrix_len:
+            raise TypeError("Each row of the matrix must have the same size")
+        for element in row:
+            if type(element) is not int and type(element) is not float:
+                raise TypeError(err_message)
+
+    if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    new_matrix = [[round(element / div, 2) for element in row] for row in matrix]
+    new_matrix = [[] for _ in range(0, len(matrix))]
+    counter = 0
+
+    for row in matrix:
+        for element in row:
+            new_matrix[counter].append(round(element / div, 2))
+        counter += 1
 
     return new_matrix
-
-# Test case
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6]
-]
-print(matrix_divided(matrix, 3))
-print(matrix)
