1ccaba48dd42d0a7d99b538024ee20897a60ba17
Sobilo34 <bilalsolih60@gmail.com>
2024-02-14 10:17:09 -0800

    script that creates the database hbtn_0d_usa and the table states (in the database

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- A script that create the database if not exists
+-- Then switch to the database
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    PRIMARY KEY(id),
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL
+) ENGINE=InnoDB;
