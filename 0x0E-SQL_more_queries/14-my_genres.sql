6fb8897d7e8fcc8ce0aeb51c28736c015e54a5ae
Sobilo34 <bilalsolih60@gmail.com>
2024-02-14 19:51:38 -0800

    14-my_genres.sql modified: added more comments

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,4 +1,9 @@
 -- A script that ists all genres of the show Dexter
+-- The tv_shows table contains only one record where title = Dexter (but the id can be different)
+-- Each record should display: tv_genres.name
+-- Results must be sorted in ascending order by the genre name
+-- can use only one SELECT statement
+-- database name will be passed as an argument of the mysql command
 
 SELECT tv_genres.name
 FROM tv_genres
