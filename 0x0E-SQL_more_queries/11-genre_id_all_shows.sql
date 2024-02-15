4bb5da8d8e9c70c1a10c5ddff20ed94c92a397c4
Sobilo34 <bilalsolih60@gmail.com>
2024-02-14 19:50:20 -0800

    11-genre_id_all_shows.sql modified: added more comments

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,8 @@
 -- A script that select all shows with genre information or display NULL if no genre
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- If a show doesn’t have a genre, display NULL
+-- can use only one SELECT statement
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
