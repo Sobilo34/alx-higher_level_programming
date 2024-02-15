68bc463a105691a3bb70a96e41b53f55c8cc5e34
Sobilo34 <bilalsolih60@gmail.com>
2024-02-14 19:53:01 -0800

    100-not_my_genres.sql modified: added some more comments

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,4 +1,9 @@
 -- A script that list genres not linked to Dexter
+-- The tv_shows table contains only one record where title = Dexter (but the id can be different)
+-- Each record should display: tv_genres.name
+-- Results must be sorted in ascending order by the genre name
+-- You can use a maximum of two SELECT statement
+-- The database name will be passed as an argument of the mysql command
 
 SELECT DISTINCT name
   FROM tv_genres AS g
