038114fc5f8a363a4239cf5697af7afdd719142f
Sobilo34 <bilalsolih60@gmail.com>
2024-02-14 19:46:56 -0800

    6-states.sql modified: added some more comments

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,7 @@
 -- A script that create the database if not exists
--- Then switch to the database
+-- states description: id INT unique, auto generated, can’t be null and is a primary key, name VARCHAR(256) can’t be null
+-- If the database hbtn_0d_usa already exists, your script should not fail
+-- If the table states already exists, your script should not fail
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
